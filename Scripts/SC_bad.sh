2395187616289aec33afcd292f27a43238d7c82d
joseflauzino <jose.wilson.vf@gmail.com>
2024-07-03 16:56:37 -0300

    Add results and bad components

---
+++ Scripts/SC_bad.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#USAGE: source SC.sh <ext_ip_iface> <ext_ip> <int_ip>
+
+curr_folder=$(pwd)
+if [[ $curr_folder =~ (.+)"/CLI"(.*) ]];
+then
+	ifconfig $1 $2
+	ip route add default dev $1
+	cd ${BASH_REMATCH[1]}/FIT-SFC_NFV-SDN-2024/Components_Bad/
+	python3 bSC.py $2 $3
+else
+	python3 bSC.py $2 $3
+fi
+
---
+++ Scripts/SFF_bad.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#USAGE: source SFF.sh <int_ip_iface> <int_ip> 
+
+curr_folder=$(pwd)
+if [[ $curr_folder =~ (.+)"/CLI"(.*) ]];
+then
+	ifconfig $1 $2
+	ip route add default dev $1
+	cd ${BASH_REMATCH[1]}/FIT-SFC_NFV-SDN-2024/Components_Bad/
+	python3 bSFF.py $2 $1
+else
+	python3 bSFF.py $2 $1
+fi
+
